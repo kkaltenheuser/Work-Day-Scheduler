7af1f129c80aecacf6319424c76c65aeb628a260
kkaltenheuser <karenkaltenheuser@gmail.com>
2020-10-09 16:24:50 -0400

    updated commits

--- script.js
+++ script.js
@@ -1,52 +1,52 @@
 $(document).ready(function () {
-  //display present day + time at moment of use
-  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-
-  //engages user through click function via saveBtn
+  // listen for save button clicks
   $(".saveBtn").on("click", function () {
-    // get neighboring values
-    console.log(this);
-    var text = $(this).siblings(".description").val();
+    // get values
+    var value = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    //sets items in localStorage
-    localStorage.setItem(time, text);
+    // save data in localStorage
+    localStorage.setItem(time, value);
   });
-  //load saved data from localStorage, do this per hour
-  $("#hour9 .description").val(localStorage.getItem("hour9"));
-  $("#hour10 .description").val(localStorage.getItem("hour10"));
-  $("#hour11 .description").val(localStorage.getItem("hour11"));
-  $("#hour12 .description").val(localStorage.getItem("hour12"));
-  $("#hour13 .description").val(localStorage.getItem("hour13"));
-  $("#hour14 .description").val(localStorage.getItem("hour14"));
-  $("#hour15 .description").val(localStorage.getItem("hour15"));
-  $("#hour16 .description").val(localStorage.getItem("hour16"));
-  $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-  function hourTracker() {
-    //identify current hour of day
-    var currentHour = moment().hour();
+  function hourUpdater() {
+    // identify current time/number of hours
+    var currentHour = moment().hours();
 
-    // loop through time blocks
+    // loop over time blocks
     $(".time-block").each(function () {
-      var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-      console.log(blockHour, currentHour);
+      var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
-      //check current time block to see if we have moved past and identify time block
+      // check to see if we've moved past the time displayed
       if (blockHour < currentHour) {
         $(this).addClass("past");
-        $(this).removeClass("future");
-        $(this).removeClass("present");
       } else if (blockHour === currentHour) {
         $(this).removeClass("past");
         $(this).addClass("present");
-        $(this).removeClass("future");
       } else {
-        $(this).removeClass("present");
         $(this).removeClass("past");
+        $(this).removeClass("present");
         $(this).addClass("future");
       }
     });
   }
-  hourTracker();
+
+  hourUpdater();
+
+  // interval to check if current time needs to be updated in viewer
+  var interval = setInterval(hourUpdater, 15000);
+
+  // load any available saved data from localStorage
+  $("#hour-9.description").val(localStorage.getItem("hour-9"));
+  $("#hour-10.description").val(localStorage.getItem("hour-10"));
+  $("#hour-11.description").val(localStorage.getItem("hour-11"));
+  $("#hour-12.description").val(localStorage.getItem("hour-12"));
+  $("#hour-13.description").val(localStorage.getItem("hour-13"));
+  $("#hour-14.description").val(localStorage.getItem("hour-14"));
+  $("#hour-15.description").val(localStorage.getItem("hour-15"));
+  $("#hour-16.description").val(localStorage.getItem("hour-16"));
+  $("#hour-17.description").val(localStorage.getItem("hour-17"));
+
+  // display present day on page the user is viewing
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
 });
